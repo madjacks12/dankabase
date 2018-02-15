8bf50235284d773dd6679ae3857bbe912ad61c02
Matt Jackson and Ryan Ferris <matt+ryan@>
2018-02-14 16:33:17 -0800

    More info url
    Create front end logic for creating the more info url for each option of
    the flavor table.

--- app/src/index-interface.js
+++ app/src/index-interface.js
@@ -1,3 +1,4 @@
+// import * as UrlIfy from './urlify.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function () {
@@ -29,11 +30,12 @@ $(document).ready(function () {
 	}, function (error) {
 		$('.showErrors').text(`There was an error processing your request: ${error.message}`);
 	});
+
 	//Strain Query
 	$('.strain-query').submit(function (event) {
 		event.preventDefault();
 		$("#table").empty();
-		$("#table").append("<tr><th>Name</th><th>Race</th><th>Flavor</th></tr>");
+		$("#table").append("<tr><th>Name</th><th>Race</th><th>Flavor</th><th>More Info</th></tr>");
 		let flavor = $('#flavor-choice option:selected').text()
 		console.log(flavor);
 
@@ -56,13 +58,17 @@ $(document).ready(function () {
 		});
 
 		strainQuery.then(function (response) {
-			let body = JSON.parse(response);
-			let i = 0;
-			for (i = 0; i < body.length; i++) {
-				$('#table').append('<tr><td>' + `${body[i].name}` + '</td><td>' + `${body[i].race}` + '</td><td>' + `${body[i].flavor}` + '</td></tr>');
-			}
-		}, function (error) {
-			$('.showErrors').text(`There was an error processing your request: ${error.message}`);
-		});
+				let i = 0;
+				let body = JSON.parse(response);
+				for (i = 0; i < body.length; i++) {
+					let removeSymbols = body[i].name.replace(/[^a-zA-Z ]/g, "");
+					let parseName = removeSymbols.replace(/\s/g, '-').toLowerCase();
+					let leaflyUrl = `https://www.leafly.com/` + `${body[i].race}` + `/` + parseName;
+					$('#table').append('<tr><td>' + `${body[i].name}` + '</td><td>' + `${body[i].race}` + '</td><td>' + `${body[i].flavor}` + '</td><td>' + "<a href=" + leaflyUrl + "><button class=btn btn-success> Learn More </button></a>" + '</td></tr>');
+				}
+			},
+			function (error) {
+				$('.showErrors').text(`There was an error processing your request: ${error.message}`);
+			});
 	}); //End of the FormId form submit event
 }); //End of the document.ready function
