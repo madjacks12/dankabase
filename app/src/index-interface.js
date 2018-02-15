cc18cc1b8f86206cd3eccc5974225ae74511c86f
Matt Jackson and Ryan Ferris <matt+ryan@>
2018-02-15 11:10:43 -0800

    jQuery Effects Dropdown
    Add the front end jQuery code for adding all the strain effects that are
    positive to the positive effects dropdownbox.

--- app/src/index-interface.js
+++ app/src/index-interface.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// const apiKey = require('./../../.env').apiKey;
-// const flavorDropdown = require('./api_calls/dropdown.flavor.js');
 import { flavorDropdown } from './api_calls/dropdown.flavors.js';
+import { effectsDropdown } from './api_calls/dropdown.effects.js';
 
 $(document).ready(function () { // jQuery Flavor dropdown
 	let flavors = flavorDropdown();
-
+	let effects = effectsDropdown();
+	// jQuery flavors dropdown
 	flavors.then(function (response) {
 		let body = JSON.parse(response);
 		let i = 0;
@@ -15,4 +15,17 @@ $(document).ready(function () { // jQuery Flavor dropdown
 	}, function (error) {
 		$('.showErrors').text(`There was an error processing your request: ${error.message}`);
 	});
+	// jQuery Effects dropdown
+	effects.then(function (response) {
+		let body = JSON.parse(response);
+		let i = 0;
+		for (i = 0; i < body.length; i++) {
+			if (body[i].type == "positive") {
+				$('#effect-choice').append(`<option value=${body[i].effect}>${body[i].effect}</option>`);
+			}
+		}
+	}, function (error) {
+		$('.showErrors').text(`There was an error processing your request: ${error.message}`);
+	});
+
 }); //End of the document.ready function
